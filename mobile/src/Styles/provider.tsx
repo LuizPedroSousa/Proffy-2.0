14cf8ba497a9f4af29576ec6f620935a536835ce
Luiz Pedro <luizpedrosousa64@gmail.com>
2020-09-26 20:59:22 -0300

    added usePersistedState

--- mobile/src/Styles/provider.tsx
+++ mobile/src/Styles/provider.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import { DefaultTheme } from 'styled-components';
+import usePersistedState from '../Utils/UsePeristedState';
 import Context from './Context';
 import dark from './themes/dark';
 import light from './themes/light';
 
 const ThemeChangeProvider: React.FC = ({ children }) => {
-    const [theme, setTheme] = useState(light);
+    const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', light);
     const toggleChangeTheme = () => {
         setTheme(theme.title === 'light' ? dark : light);
     }
